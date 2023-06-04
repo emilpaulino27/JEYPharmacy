b08dda64b7aca5fe4a9465c12bc9b9e6e1d31e2a
emilpaulino27 <2190023@ipisa.edu.do>
2023-06-04 08:28:43 -0400

    Arreglos minimos

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,16 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
 
 Auth::routes();
 
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 //Route::get('/', [HomeController::class, 'index']);
 
 //Route::get('/redirect', [HomeController::class, 'redirect']);
-
-/*Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');*/
-
 
 Route::get('/', function () {
     return view('home.userpage');
